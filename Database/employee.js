7cdf0cd5ed70230b06b832159f4334559a79a0bd
Rajat Cambo <rajatcambo7@gmail.com>
2019-10-01 10:33:01 +0530

    added new field

--- Database/employee.js
+++ Database/employee.js
@@ -2,6 +2,8 @@ var mongoose=require("mongoose")
 var employee=new mongoose.Schema({
     first_name:{type:String},
     last_name:{type:String},
+    father_name:{type:String},
+    mother_name:{type:String},
     gender:{type:String},
     email:{type:String},
     address:{type:String},
